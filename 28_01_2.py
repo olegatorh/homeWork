c3cdf561de25d68c324a135cd9da0b144c094df2
olegatorh <herston4ik@gmail.com>
2020-02-23 11:19:11 +0200

    homework N2

---
+++ 28_01_2.py
@@ -0,0 +1,4 @@
+name = input("What is your name?   ")
+age = input("How old are you?   ")
+place = input("Where do you live?   ")
+print("Hello {}, Your age is {}, You live in {}.".format(name, age, place))
